75342dec1d883655317e0fcc8c16747f2e64bad5
Tania <bystrovat@procoders.tech>
2023-09-04 23:32:44 +0300

    hw conditions and loops

---
+++ js_conditions_and_loops/task3.js
@@ -0,0 +1,12 @@
+const number = 8
+
+for (let i = 1; i <= 10; i++) {
+   console.log(`${number} X ${i} = `+ number * i)  
+}
+
+
+let i = 1
+while (i <= 10) {
+    console.log(`${number} X ${i} = `+ number * i)
+    i++  
+}
