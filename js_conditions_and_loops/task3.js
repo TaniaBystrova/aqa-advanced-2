fb57befc1498b40179d3cd464f5c190a7300dc01
Tania <bystrovat@procoders.tech>
2023-09-05 10:47:42 +0300

    added separator, fixed expression

--- js_conditions_and_loops/task3.js
+++ js_conditions_and_loops/task3.js
@@ -1,12 +1,13 @@
 const number = 8
 
 for (let i = 1; i <= 10; i++) {
-   console.log(`${number} X ${i} = `+ number * i)  
+   console.log(`${number} X ${i} = ${number * i}`)  
 }
 
+console.log("||-||-||-||-||")
 
 let i = 1
 while (i <= 10) {
-    console.log(`${number} X ${i} = `+ number * i)
+    console.log(`${number} X ${i} = ${number * i}`)
     i++  
 }
