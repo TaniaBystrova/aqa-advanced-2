a3037f4083eaacca684089cd775266a57b7c0e49
Tania <bystrovat@procoders.tech>
2023-09-12 14:29:10 +0300

    fix task 3

--- js_functions/task3.js
+++ js_functions/task3.js
@@ -1,16 +1,8 @@
 function counter(num) {
     console.log(num)
     if (num > 1) {
-        return counter(num - 1) 
-   // } else {
-       // return "Stop function!"
-   // }
-}
+        return counter(num - 1)
+    }
 }
 
-const result = counter(5)
-console.log(result)
-
-//function counter(num) {
-   // console.log(num)
-   // if (num > 1) {
+counter(5)
